d60b4590cc35a7a88484a5fa30ea5163c2963b2e
João Victor Ipirajá de Alencar <joaovictorpiraja@gmail.com>
2023-05-19 15:34:50 -0300

    Update noisecancelling.c

--- noisecancelling.c
+++ noisecancelling.c
@@ -1,165 +1,261 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 
 #define M_PI 3.14159265358979323846
 
+#define INITIAL_MAX_RECORDS 100
+#define MAX_FIELD_LENGTH 20
 
-/* Função que retorna o coeficiente b */
-void butter_b_coeffs(int order, double cutoff_freq, double *b_coeffs) {
-    double fc = tan(cutoff_freq * M_PI) ;
-    double denom;
+typedef struct {
+    double *values;
+    int size;
+} CSVData;
 
-    for(int i = 0; i <= order; i++) {
-        b_coeffs[i] = 0.0;
-        for(int j = 0; j <= order; j++) {
-            if (i == j) {
-                denom = 1.0;
-            } else if (i == j + 1 || i == j - 1) {
-                denom = order * fc;
-            } else {
-                denom = 0.0;
+
+typedef struct {
+    double b0, b1, b2;
+    double a1, a2;
+} FilterCoefficients;
+
+
+CSVData processCSVFile(const char *filename) {
+    CSVData csvData;
+    FILE *file;
+    char *line = NULL;
+    char *field;
+    double *values = NULL;
+    int recordCount = 0;
+    int maxRecords = 0;
+    size_t lineLength = 0;
+    size_t maxLineLength = 0;
+
+    // Abre o arquivo CSV para leitura
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.");
+        csvData.values = NULL;
+        csvData.size = 0;
+        return csvData;
+    }
+
+    // Lê linha por linha do arquivo
+    while (getline(&line, &lineLength, file) != -1) {
+        field = strtok(line, ",");
+        while (field != NULL) {
+            double value = atof(field);
+
+            // Realoca o array de valores se necessário
+            if (recordCount >= maxRecords) {
+                maxRecords = (maxRecords == 0) ? INITIAL_MAX_RECORDS : maxRecords * 2;
+                values = realloc(values, maxRecords * sizeof(double));
+                if (values == NULL) {
+                    printf("Erro ao alocar memória.");
+                    fclose(file);
+                    free(line);
+                    csvData.values = NULL;
+                    csvData.size = 0;
+                    return csvData;
+                }
             }
-            b_coeffs[i] += pow(-1.0, i+j) * fc / (1.0 + pow(fc, 2.0)) * denom;
+
+            values[recordCount] = value;
+            recordCount++;
+
+            field = strtok(NULL, ",");
+        }
+
+        // Atualiza o tamanho máximo da linha, se necessário
+        size_t currentLineLength = strlen(line);
+        if (currentLineLength > maxLineLength) {
+            maxLineLength = currentLineLength;
+            line = realloc(line, (maxLineLength + 1) * sizeof(char));
+            if (line == NULL) {
+                printf("Erro ao alocar memória.");
+                fclose(file);
+                free(values);
+                csvData.values = NULL;
+                csvData.size = 0;
+                return csvData;
+            }
         }
     }
+
+    csvData.values = values;
+    csvData.size = recordCount;
+
+    // Libera a memória alocada para a linha
+    free(line);
+
+    // Fecha o arquivo
+    fclose(file);
+
+    return csvData;
 }
 
-/* Função que retorna o coeficiente a */
-void butter_a_coeffs(int order, double *a_coeffs) {
-    double denom;
-
-    for(int i = 0; i <= order; i++) {
-        a_coeffs[i] = 0.0;
-        for(int j = 0; j <= order; j++) {
-            if (i == j) {
-                denom = 1.0;
-            } else if (i == j + 1 || i == j - 1) {
-                denom = order;
-            } else {
-                denom = 0.0;
-            }
-            a_coeffs[i] += pow(-1.0, i+j) * denom;
-        }
+double dotProduct(const double *a, const double *b, int length) {
+    double result = 0.0;
+    for (int i = 0; i < length; i++) {
+        result += a[i] * b[i];
     }
+    return result;
 }
 
-/* Função de filtro Butterworth */
-void signal_butter(int order, double cutoff_freq, double *input, double *output, int length) {
-    double b_coeffs[order+1];
-    double a_coeffs[order+1];
-
-    butter_b_coeffs(order, cutoff_freq, b_coeffs);
-    butter_a_coeffs(order, a_coeffs);
-
-    double sum;
-
-    for(int i = 0; i < length; i++) { //int i
-        sum = 0.0;
-        for(int j = 0; j <= order; j++) {
-            if(i - j < 0) {
-                sum += 0.0;
-            } else {
-                sum += b_coeffs[j] * input[i-j];
-            }
-        }
-        for(int j = 1; j <= order; j++) {
-            if(i - j < 0) {
-                sum += 0.0;
-            } else {
-                sum -= a_coeffs[j] * output[i-j];
-            }
-        }
-        output[i] = sum / a_coeffs[0];
-    }
-}
-
-void signal_filtfilt(float *x, float *y, int N, float *b, int nb, float *a, int na) {
-    // N: comprimento do sinal de entrada
-    // x: sinal de entrada
-    // y: sinal de saída
-    // b: coeficientes do numerador do filtro
-    // nb: ordem do numerador do filtro
-    // a: coeficientes do denominador do filtro
-    // na: ordem do denominador do filtro
-
-    int i, j;
-    float sum;
-
-    // Inicializar saídas
-    for (i = 0; i < N; i++) {
-        y[i] = 0;
-    }
-
-    // Aplicar filtro
-    for (i = 0; i < N; i++) {
-        sum = 0;
-        for (j = 0; j < nb; j++) {
+void forwardFilter(const double *input, const double *b, const double *a,double *output, int size, int ncoef) {
+    for (int i = 0; i < size; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < ncoef; j++) {
             if (i - j >= 0) {
-                sum += b[j] * x[i - j];
+                sum += b[j] * input[i - j];
             }
         }
-        for (j = 1; j < na; j++) {
+        for (int j = 1; j < ncoef; j++) {
             if (i - j >= 0) {
-                sum -= a[j] * y[i - j];
+                sum -= a[j] * output[i - j];
             }
         }
-        y[i] = sum / a[0];
+        output[i] = sum / a[0];
     }
 }
 
+void backwardFilter(const double *input, const double *b, const double *a, double *output, int size, int ncoef) {
+    for (int i = size - 1; i >= 0; i--) {
+        double sum = 0.0;
+        for (int j = 0; j < ncoef; j++) {
+            if (i + j < size) {
+                sum += b[j] * input[i + j];
+            }
+        }
+        for (int j = 1; j < ncoef; j++) {
+            if (i + j < size) {
+                sum -= a[j] * output[i + j];
+            }
+        }
+        output[i] = sum / a[0];
+    }
+}
+
+void signal_filtfilt(const double *b, const double *a, const double *x, double *y, int size, int ncoef) {
+    double *temp = (double *)malloc(size * sizeof(double));
+    double *reverse = (double *)malloc(size * sizeof(double));
+
+    forwardFilter(x, b, a, temp, size, ncoef);
+    backwardFilter(temp, b, a, reverse, size, ncoef);
+    forwardFilter(reverse, b, a, y, size, ncoef);
+
+    free(temp);
+    free(reverse);
+}
 
 
-void nlms_filter_with_lowpass(float *x, float *d, int L, float mu, float beta, float pre_emphasis, float cutoff_freq, int len_x, float *y)
-{
-    // Apply a lowpass filter to the input signal
-    float *x_filtered = (float*) malloc(len_x * sizeof(float));
-    int i;
-    float a[5], b[5];
+void butterworthLowpass(int order, double cutoff, double *b, double *a) {
+    double omegaC = tan(M_PI * cutoff);
+    double omegaCSquared = omegaC * omegaC;
+    double theta = M_PI / (2.0 * order);
+
+    // Calculate the denominator coefficients (a)
+    for (int i = 0; i < order; i++) {
+        double alpha = sin((2 * i + 1) * theta);
+        double beta = 1.0 + (omegaC * alpha);
+        a[i] = omegaCSquared / (beta + omegaCSquared);
+    }
+
+    // Calculate the numerator coefficients (b)
+    for (int i = 0; i < order; i++) {
+        b[i] = 1.0;
+    }
+}
+
+void nlmsFilterWithLowpass(const double *x, const double *d, int length,   double *y) {
+
+    int L = 256;
+    double mu = 0.0001;
+    double beta = 1.0;
+  
     int order = 4;
-    signal_butter(order, cutoff_freq, b, a, len_x);
-    signal_filtfilt(b, a, x, len_x, x_filtered, a, b);
+    double cutoff = 0.75;
+    double pre_emphasis = 0.35;
+    double energy = 0.1;
+
+  
+    double *x_filtered = (double *)malloc(length * sizeof(double));
+    double *x_pre_emph = (double *)malloc(length * sizeof(double));
+    double *w = (double *)malloc(L * sizeof(double));
+    double *buffer = (double *)malloc(L * sizeof(double));
+    double *b = (double *)malloc(order * sizeof(double));
+    double *a = (double *)malloc(order * sizeof(double));
+
+    butterworthLowpass(order,cutoff,b, a);
+    signal_filtfilt(b,a, x, x_filtered, L, order);
+
+
+    // Apply lowpass filter to the input signal
+    for (int i = 0; i < length; i++) {
+        if (i == 0) {
+            x_filtered[i] = x[i];
+        } else {
+            x_filtered[i] = (1 - cutoff) * x_filtered[i - 1] + cutoff * x[i];
+        }
+    }
 
     // Apply pre-emphasis to the filtered input signal
-    float *x_pre_emph = (float*) malloc(len_x * sizeof(float));
     x_pre_emph[0] = x_filtered[0];
-    for (i = 1; i < len_x; i++) {
-        x_pre_emph[i] = x_filtered[i] - pre_emphasis * x_filtered[i-1];
+    for (int i = 1; i < length; i++) {
+        x_pre_emph[i] = x_filtered[i] - pre_emphasis * x_filtered[i - 1];
     }
 
-    // Initialize the filter coefficients and energy estimate
-    float *w = (float*) calloc(L, sizeof(float));
-    float energy = 0.1f;
-
-    // Create a buffer to store the previous input samples
-    float *buffer = (float*) calloc(L, sizeof(float));
+    // Initialize the filter coefficients
+    for (int i = 0; i < L; i++) {
+        w[i] = 0.0;
+    }
 
     // Apply the NLMS filter to the input signal
-    for (i = 0; i < len_x; i++) {
+    for (int n = 0; n < length; n++) {
         // Update the buffer with the latest input sample
-        memmove(buffer + 1, buffer, (L - 1) * sizeof(float));
-        buffer[0] = x_pre_emph[i];
-
-        // Calculate the output signal and error
-        float output = 0.0f;
-        int j;
-        for (j = 0; j < L; j++) {
-            output += w[j] * buffer[j];
+        for (int i = L - 1; i > 0; i--) {
+            buffer[i] = buffer[i - 1];
         }
-        y[i] = output;
+        buffer[0] = x_pre_emph[n];
 
-        float error = d[i] - output;
+        // Calculate the output signal
+        y[n] = 0.0;
+        for (int i = 0; i < L; i++) {
+            y[n] += w[i] * buffer[i];
+        }
+
+        // Calculate the error
+        double e;
+        if (n < length) {
+            e = d[n] - y[n];
+        } else {
+            e = 0.0;
+        }
 
         // Update the filter coefficients and energy estimate
-        energy = beta * energy + (1.0f - beta) * dot_product(buffer, buffer, L);
-
-        for (j = 0; j < L; j++) {
-            w[j] += (mu / energy) * error * buffer[j];
+        energy = beta * energy + (1 - beta) * dotProduct(buffer, buffer, L);
+        for (int i = 0; i < L; i++) {
+            w[i] += (mu / energy) * e * buffer[i];
         }
     }
+}
 
-    free(x_filtered);
-    free(x_pre_emph);
-    free(w);
-    free(buffer);
+
+
+
+int main() {
+
+   int order = 4;
+   double cutoff = 0.75;
+  //  const char *filename = "audio_com_ruido.csv";
+    CSVData audioRuido = processCSVFile("audio_ruido.csv");
+    CSVData audioComRuido = processCSVFile("audio_com_ruido.csv");
+
+    double *y = (double*) malloc(audioRuido.size *             sizeof(double));
+ 
+    nlmsFilterWithLowpass(audioRuido.values, audioComRuido.values,   audioRuido.size, y);
+
+
+    return 0;
 }
