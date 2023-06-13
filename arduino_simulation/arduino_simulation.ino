03684e49d344c4dbf3902f849b77a24541dbd4b8
joaoipiraja <joaovictorpiraja@gmail.com>
2023-06-13 14:22:09 -0300

    Improvements

--- arduino_simulation/arduino_simulation.ino
+++ arduino_simulation/arduino_simulation.ino
@@ -5,12 +5,19 @@
 #define LENGHT 10 //Size of a Batch
 #define FILTER_LENGHT 100 // Filter Lenght 
 
+
+static const long reference_signal[] = {-0.06077251, -0.03236705, -0.07158277,  0.03226485,  0.15505562,
+         0.02220206,  0.1239529 ,  0.06949102,  0.3687647 , -0.12777717};
+
+static const long ruido[] = {-0.06077251, -0.03865648, -0.08416139,  0.01339755,  0.12990038,
+        -0.00924013,  0.086225  ,  0.02547892,  0.31847013, -0.18435222};
 // Arrays and variables
-float filter_coefficients[FILTER_LENGHT];
-float buffer[FILTER_LENGHT];
-float output[LENGHT];     // Output
-float reference_signal[LENGHT];     // Input with noise
-float ruido[LENGHT]; // Noise
+long filter_coefficients[FILTER_LENGHT];
+long buffer[FILTER_LENGHT];
+long output[LENGHT];     // Output
+
+unsigned long startTime = 0.0;
+unsigned long endTime = 0.0;
 
 // Parameters
 float step_size;
@@ -18,17 +25,38 @@ double energy;
 float error;
 float result;
 
+
+//soma em árvore (tree-based summation):
 // Calculates the dot product of two arrays
-float dotProduct(float a[], float b[], int size) {
-    result = 0.0;
+
+// long dotProduct(long a[], long b[], int size) {
+//     long result = 0.0;
+
+//     // Loop otimizado para processar elementos em pares
+//     for (int i = 0; i < size - 1; i += 2) {
+//         result += a[i] * b[i] + a[i + 1] * b[i + 1];
+//     }
+
+//     // Processa elemento restante, se houver
+//     if (size % 2 != 0) {
+//         result += a[size - 1] * b[size - 1];
+//     }
+
+//     return result;
+// }
+
+long dotProduct(long a[], long b[], int size) {
+    long result = 0.0;
     for (int i = 0; i < size; i++) {
-        result += a[i] * b[i];
+        result += static_cast<int>(a[i]) * static_cast<int>(b[i]);
     }
     return result;
 }
 
+
+
 // Initializes an array with zeros
-void initArray(float values[], int length) {
+void initArray(long values[], int length) {
     for (int i = 0; i < length; i++) {
         values[i] = 0.0;
     }
@@ -41,9 +69,11 @@ void nlms_filter() {
     // Apply the NLMS filter to the input signal
     for (int n = 0; n < LENGHT; n++) {
         // Shift the filter coefficients
+      
         for (int i = FILTER_LENGHT- 1; i > 0; i--) {
             buffer[i] = buffer[i - 1];
         }
+        
         buffer[0] = reference_signal[n];
 
         // Calculate the output
@@ -56,7 +86,7 @@ void nlms_filter() {
         // Update the filter coefficients and energy estimate
         energy = energy + dotProduct(buffer, buffer, FILTER_LENGHT);
         for (int i = 0; i < FILTER_LENGHT; i++) {
-            filter_coefficients[i] += (step_size/ energy) * error * buffer[i];
+            filter_coefficients[i] += (step_size / energy) * error * buffer[i];
         }
     }
 }
@@ -65,44 +95,29 @@ void setup() {
     // Open a serial connection
     Serial.begin(31250);
 
-    step_size = 0.0001;
+    step_size = 0.001;
     energy = 0.1;
     // Initialize arrays with zeros
     initArray(filter_coefficients, FILTER_LENGHT);
     initArray(buffer, FILTER_LENGHT);
 
-    pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
 
-    initArray(reference_signal, LENGHT);
-    initArray(ruido, LENGHT);
+    startTime = 0.0;
+    endTime = 0.0;
     initArray(output, LENGHT);
     
-    
-    // Wait until data is available on the serial connection
-    
-    while (!Serial.available()) {
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
+    //while (!Serial.available()) {}
 
-    // Read input values from serial
-    for (int i = 0; i < LENGHT; i++) {
-        reference_signal[i] = Serial.parseFloat();
-    }
+    startTime = millis();
+    nlms_filter(); // 58 ms
+    //Com long ao invés de float - 48ms
 
-    // Read noise values from serial
-    for (int i = 0; i < LENGHT; i++) {
-        ruido[i] = Serial.parseFloat();
-    }
+    endTime = millis();
 
-    // Apply the NLMS Adaptative filter
-    nlms_filter();
+    Serial.println(endTime - startTime);
 
-    // Print the output values
-    for (int i = 0; i < LENGHT; i++) {
-        Serial.println(output[i], 6);
-    }
 }
---
+++ arduino_simulation_optimization/arduino_simulation_measure.ino
@@ -0,0 +1,108 @@
+#include <string.h>
+#include <math.h>
+
+// Constants
+#define LENGHT 10 //Size of a Batch
+#define FILTER_LENGHT 100 // Filter Lenght 
+
+// Arrays and variables
+float filter_coefficients[FILTER_LENGHT];
+float buffer[FILTER_LENGHT];
+float output[LENGHT];     // Output
+float reference_signal[LENGHT];     // Input with noise
+float ruido[LENGHT]; // Noise
+
+// Parameters
+float step_size;
+double energy;
+float error;
+float result;
+
+// Calculates the dot product of two arrays
+float dotProduct(float a[], float b[], int size) {
+    result = 0.0;
+    for (int i = 0; i < size; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+
+// Initializes an array with zeros
+void initArray(float values[], int length) {
+    for (int i = 0; i < length; i++) {
+        values[i] = 0.0;
+    }
+}
+
+// Applies the NLMS Adaptative filter
+void nlms_filter() {
+    error = 0.0;  
+
+    // Apply the NLMS filter to the input signal
+    for (int n = 0; n < LENGHT; n++) {
+        // Shift the filter coefficients
+        for (int i = FILTER_LENGHT- 1; i > 0; i--) {
+            buffer[i] = buffer[i - 1];
+        }
+        buffer[0] = reference_signal[n];
+
+        // Calculate the output
+        output[n] = dotProduct(filter_coefficients, buffer, FILTER_LENGHT);
+
+        // Calculate the error
+      
+        error = reference_signal[n] - output[n];
+
+        // Update the filter coefficients and energy estimate
+        energy = energy + dotProduct(buffer, buffer, FILTER_LENGHT);
+        for (int i = 0; i < FILTER_LENGHT; i++) {
+            filter_coefficients[i] += (step_size/ energy) * error * buffer[i];
+        }
+    }
+}
+
+void setup() {
+    // Open a serial connection
+    Serial.begin(31250);
+
+    step_size = 0.0001;
+    energy = 0.1;
+    // Initialize arrays with zeros
+    initArray(filter_coefficients, FILTER_LENGHT);
+    initArray(buffer, FILTER_LENGHT);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+
+    initArray(reference_signal, LENGHT);
+    initArray(ruido, LENGHT);
+    initArray(output, LENGHT);
+    
+    
+    // Wait until data is available on the serial connection
+    
+    while (!Serial.available()) {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    // Read input values from serial
+    for (int i = 0; i < LENGHT; i++) {
+        reference_signal[i] = Serial.parseFloat();
+    }
+
+    // Read noise values from serial
+    for (int i = 0; i < LENGHT; i++) {
+        ruido[i] = Serial.parseFloat();
+    }
+
+    // Apply the NLMS Adaptative filter
+    nlms_filter();
+
+    // Print the output values
+    for (int i = 0; i < LENGHT; i++) {
+        Serial.println(output[i], 6);
+    }
+}
